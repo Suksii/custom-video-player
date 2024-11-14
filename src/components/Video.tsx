49a4fed8549ad5c326f0a58065791633c59da861
unknown <ramovic225@gmail.com>
2024-11-14 23:34:41 +0100

    full screen toggle added

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,12 @@
 import { useRef, useState } from "react";
 import videoTest from "../assets/video-test.mp4";
+import { MdFullscreen, MdOutlineFullscreenExit } from "react-icons/md";
 import { FaPlay, FaPause } from "react-icons/fa";
 
 const Video = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
 
   const handlePlayPause = () => {
     if (videoRef.current) {
@@ -12,12 +14,31 @@ const Video = () => {
       setIsPlaying(!isPlaying);
     }
   };
+
+  const handleFullScreen = () => {
+    setIsFullScreen((prev) => !prev);
+  };
   return (
-    <div className="aspect-video w-[800px] relative">
-      <video ref={videoRef} src={videoTest} width="100%" height="auto"></video>
-      <div className="absolute h-8 bg-black bg-opacity-50 bottom-0 w-full px-4 flex items-center border-t border-black">
+    <div
+      onDoubleClick={handleFullScreen}
+      className={` ${
+        isFullScreen ? "w-full h-screen bg-black bg-opacity-70" : "w-[800px]"
+      } relative aspect-video`}
+    >
+      <video
+        ref={videoRef}
+        src={videoTest}
+        className="w-full h-full object-contain"
+      ></video>
+      <div className="absolute h-8 bg-black bg-opacity-50 bottom-0 w-full px-4 flex items-center justify-between">
         <div onClick={handlePlayPause} className="text-gray-100 cursor-pointer">
           {isPlaying ? <FaPause /> : <FaPlay />}
+        </div>
+        <div
+          onClick={handleFullScreen}
+          className="text-gray-100 cursor-pointer"
+        >
+          {isFullScreen ? <MdOutlineFullscreenExit /> : <MdFullscreen />}
         </div>
       </div>
     </div>
